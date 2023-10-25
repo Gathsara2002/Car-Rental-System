32e69da1114f5e75d7042e2cee682bb65e60c594
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-25 21:08:00 +0530

    Content updated

--- Front_End/controllers/loginController.js
+++ Front_End/controllers/loginController.js
@@ -1,3 +1,27 @@
+let userName;
+let passWord;
+let role;
+
 $("#btnLogin").click(function () {
-    alert("working");
+
+    userName = $("#username").val();
+    passWord = $("#password").val();
+    role = $("#role_Type").val();
+    checkUser();
+    clearAll();
+
 });
+
+
+function checkUser() {
+
+    /*get all users*/
+
+    /*check user*/
+}
+
+function clearAll() {
+    $("#username").val("");
+    $("#password").val("");
+    $("#role_Type").val("");
+}
