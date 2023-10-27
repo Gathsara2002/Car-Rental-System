0f72c346eda61510d2103285e984a9e057c85389
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-27 20:01:25 +0530

    LoginServiceImpl added

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/impl/LoginServiceImpl.java
@@ -0,0 +1,37 @@
+package lk.easy_car_rental.service.impl;
+
+import lk.easy_car_rental.dto.LoginDTO;
+import lk.easy_car_rental.service.LoginService;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/27/2023 -- 8:00 PM
+ **/
+public class LoginServiceImpl implements LoginService {
+    @Override
+    public void addUser(LoginDTO dto) {
+
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+    }
+
+    @Override
+    public List<LoginDTO> getAllUser() {
+        return null;
+    }
+
+    @Override
+    public LoginDTO findUser(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(LoginDTO dto) {
+
+    }
+}
