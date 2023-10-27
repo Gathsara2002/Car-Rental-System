c3735ca241d2664fbafaa653cb4c783ebe3f13d7
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-27 19:42:47 +0530

    Added LoginDTO class

---
+++ Back_End/src/main/java/lk/easy_car_rental/dto/LoginDTO.java
@@ -0,0 +1,16 @@
+package lk.easy_car_rental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : Gathsara
+ * created : 10/27/2023 -- 7:42 PM
+ **/
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class LoginDTO {
+}
