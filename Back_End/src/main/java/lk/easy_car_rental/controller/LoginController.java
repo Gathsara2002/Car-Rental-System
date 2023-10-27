94c8821c1d84a246f73ac014115b65bb40c7f05b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-27 19:40:27 +0530

    Addd LoginController to control user login process

---
+++ Back_End/src/main/java/lk/easy_car_rental/controller/LoginController.java
@@ -0,0 +1,14 @@
+package lk.easy_car_rental.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Gathsara
+ * created : 10/27/2023 -- 7:38 PM
+ **/
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+}
