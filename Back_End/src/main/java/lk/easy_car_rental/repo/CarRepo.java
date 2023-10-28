3f7b2afa092eb741f120f9ba61ec3cc13034ce9a
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 19:47:59 +0530

    Added car repo

---
+++ Back_End/src/main/java/lk/easy_car_rental/repo/CarRepo.java
@@ -0,0 +1,10 @@
+package lk.easy_car_rental.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : Gathsara
+ * created : 10/28/2023 -- 7:47 PM
+ **/
+public interface CarRepo extends JpaRepository<Car, String> {
+}
