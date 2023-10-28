6c2b81503c8bcc190f792e354b78ddb0e6e71205
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 20:47:45 +0530

    Added DriverDto class

---
+++ Back_End/src/main/java/lk/easy_car_rental/dto/DriverDTO.java
@@ -0,0 +1,18 @@
+package lk.easy_car_rental.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Gathsara
+ * created : 10/28/2023 -- 8:47 PM
+ **/
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class DriverDTO {
+}
