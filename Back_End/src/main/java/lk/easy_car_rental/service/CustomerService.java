8411944623a0cd3021ebfe948cbc7561965a52e6
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 13:52:12 +0530

    Create customer service

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/CustomerService.java
@@ -0,0 +1,24 @@
+package lk.easy_car_rental.service;
+
+import lk.easy_car_rental.dto.CustomerDTO;
+import lk.easy_car_rental.dto.LoginDTO;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/28/2023 -- 1:51 PM
+ **/
+public interface CustomerService {
+
+    void addCustomer(CustomerDTO dto);
+
+    void deleteCustomer(String id);
+
+    List<CustomerDTO> getAllCustomer();
+
+    CustomerDTO findCustomer(String id);
+
+    void updateCustomer(CustomerDTO dto);
+
+}
