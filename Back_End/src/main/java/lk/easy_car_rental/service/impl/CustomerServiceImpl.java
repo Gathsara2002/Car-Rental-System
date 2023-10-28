1b732819fafbe2e6d91111a7453e95f5bf578cfd
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 13:52:58 +0530

    Create customer service impl class

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package lk.easy_car_rental.service.impl;
+
+import lk.easy_car_rental.dto.CustomerDTO;
+import lk.easy_car_rental.service.CustomerService;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/28/2023 -- 1:52 PM
+ **/
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void addCustomer(CustomerDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO findCustomer(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO dto) {
+
+    }
+}
