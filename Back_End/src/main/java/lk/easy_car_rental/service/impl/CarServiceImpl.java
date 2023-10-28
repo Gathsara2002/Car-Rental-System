314fde5484f89a767eff9ae90941c3cd50c29ea7
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 19:50:13 +0530

    Added CarServiceImpl class

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/impl/CarServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.easy_car_rental.service.impl;
+
+import lk.easy_car_rental.service.CarService;
+
+/**
+ * @author : Gathsara
+ * created : 10/28/2023 -- 7:49 PM
+ **/
+public class CarServiceImpl implements CarService {
+}
