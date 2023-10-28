dc23854fd92c9c064cb2824f7520ae22adc19466
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 12:55:06 +0530

    User login function is working

--- Front_End/controllers/loginController.js
+++ Front_End/controllers/loginController.js
@@ -1,13 +1,21 @@
 let userName;
 let passWord;
 let role;
+let user;
 
 $("#btnLogin").click(function () {
 
     userName = $("#username").val();
     passWord = $("#password").val();
     role = $("#role_Type").val();
-    checkUser();
+
+    let isUserChecked = checkUser();
+    if (isUserChecked) {
+        console.log("user is reg");
+    } else {
+        console.log("user is not reg");
+    }
+
     clearAll();
 
 });
@@ -16,12 +24,35 @@ $("#btnLogin").click(function () {
 function checkUser() {
 
     /*get all users*/
+    $.ajax({
+        url: BaseUrl + "login",
+        dataType: "json",
+        async: false,
+        success: function (resp) {
+            user = resp.data;
+        },
+        error: function (error) {
+            alert(error.responseJSON.message)
+        }
+    });
 
     /*check user*/
+    for (let i in user) {
+        let u = user[i];
+        console.log(u)
+        let passWord1 = u.passWord;
+        let userName1 = u.userName;
+        let role1 = u.role;
+
+        if (userName === userName1 && passWord === passWord1 && role === role1) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function clearAll() {
     $("#username").val("");
     $("#password").val("");
-    $("#role_Type").val("");
+    $("#role_Type").val("Choose Role");
 }
