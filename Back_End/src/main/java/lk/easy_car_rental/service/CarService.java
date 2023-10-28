b340deff51807341944d47ed1a6ee49fb2ec71ac
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-28 20:26:58 +0530

    Car service updated

--- Back_End/src/main/java/lk/easy_car_rental/service/CarService.java
+++ Back_End/src/main/java/lk/easy_car_rental/service/CarService.java
@@ -1,8 +1,23 @@
 package lk.easy_car_rental.service;
 
+import lk.easy_car_rental.dto.CarDTO;
+
+import java.util.List;
+
 /**
  * @author : Gathsara
  * created : 10/28/2023 -- 7:48 PM
  **/
 public interface CarService {
+
+    void addCar(CarDTO dto);
+
+    void deleteCar(String id);
+
+    List<CarDTO> getAllCar();
+
+    CarDTO findCar(String id);
+
+    void updateCar(CarDTO dto);
+
 }
--- Back_End/src/main/java/lk/easy_car_rental/service/impl/CarServiceImpl.java
+++ Back_End/src/main/java/lk/easy_car_rental/service/impl/CarServiceImpl.java
@@ -1,10 +1,37 @@
 package lk.easy_car_rental.service.impl;
 
+import lk.easy_car_rental.dto.CarDTO;
 import lk.easy_car_rental.service.CarService;
+
+import java.util.List;
 
 /**
  * @author : Gathsara
  * created : 10/28/2023 -- 7:49 PM
  **/
 public class CarServiceImpl implements CarService {
+    @Override
+    public void addCar(CarDTO dto) {
+
+    }
+
+    @Override
+    public void deleteCar(String id) {
+
+    }
+
+    @Override
+    public List<CarDTO> getAllCar() {
+        return null;
+    }
+
+    @Override
+    public CarDTO findCar(String id) {
+        return null;
+    }
+
+    @Override
+    public void updateCar(CarDTO dto) {
+
+    }
 }
