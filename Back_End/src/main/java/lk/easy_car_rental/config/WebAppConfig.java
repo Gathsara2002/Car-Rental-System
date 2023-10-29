dcc8512971ee4ba39219a3fe749692353fc462d4
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-29 19:12:09 +0530

    Added multipart resolver to context

--- Back_End/src/main/java/lk/easy_car_rental/config/WebAppConfig.java
+++ Back_End/src/main/java/lk/easy_car_rental/config/WebAppConfig.java
@@ -1,7 +1,10 @@
 package lk.easy_car_rental.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,6 +20,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig() {
         System.out.println("WebAppConfig: Instantiated");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(500000000);
+        return multipartResolver;
     }
 
     @Override
