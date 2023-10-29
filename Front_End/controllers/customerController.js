d3c5b1a9248b2367ef7b533a1feee4ff5e63c7be
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-29 20:58:25 +0530

    Front end updated

--- Front_End/controllers/customerController.js
+++ Front_End/controllers/customerController.js
@@ -1,4 +1,21 @@
 /*Register Customer*/
+
 $("#btnCusRegister").click(function () {
 
+    let formData = new FormData($("#customerRegForm")[0]);
+    console.log(formData);
+
+    $.ajax({
+        url: BaseUrl + "customer",
+        method: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (resp) {
+            //alert(resp.message);
+        },
+        error: function (error) {
+            // alert(error.message);
+        }
+    });
 });
