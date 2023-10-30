d1c790b789c705b08e29edf1c7b295636d726dcf
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-30 21:13:37 +0530

    Added regex to customer

---
+++ Front_End/controllers/validation/customerValidation.js
@@ -0,0 +1,7 @@
+//regex
+const CUS_NAME_REGEX = /^[A-Za-z ]{4,}$/;
+const CUS_ADDRESS_REGEX = /^[A-Za-z0-9 ]{5,}$/;
+const CUS_TEL_REGEX = /^[0-9]{2,}$/;
+const CUS_EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const CUS_NIC_REGEX = /^([0-9]{12}|[0-9V]{10})$/;
+const CUS_LICENSE_REGEX = /^[A-Z0-9-]+$/;
