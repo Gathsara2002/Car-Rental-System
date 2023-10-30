a492a6ef6a1277dcfa3f4437a5220feecf68fb5b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-30 12:08:01 +0530

    Created get all drivers function

--- Front_End/controllers/driverController.js
+++ Front_End/controllers/driverController.js
@@ -1,6 +1,6 @@
 /*save driver*/
-function saveDriver(){
-   let formData = $("#driverForm").serialize();
+function saveDriver() {
+    let formData = $("#driverForm").serialize();
 
     $.ajax({
         url: BaseUrl + "diver",
@@ -13,6 +13,39 @@ function saveDriver(){
         },
         error: function (error) {
             alert(error.message);
+        }
+    });
+}
+
+/*getAllDrivers*/
+function getAllDrivers() {
+
+    $("#tblDriver").empty();
+
+    $.ajax({
+        url: BaseUrl + 'driver',
+        dataType: "json",
+        success: function (response) {
+            let drivers = response.data;
+            for (let i in drivers) {
+                let dr = drivers[i];
+                let id = dr.userId;
+                let name = dr.name;
+                let address = dr.address;
+                let contact = dr.contact;
+                let license = dr.license;
+                let availability = dr.availability;
+                let userName = dr.userName;
+                let passWord = dr.passWord;
+                let row = `<tr><td>${id}</td><td>${name}</td><td>${contact}</td><td>${address}</td><td>${license}</td>
+                            <td>${availability}</td><td>${userName}</td><td>${passWord}</td></tr>`;
+
+                $("#tblDriver").append(row);
+            }
+            bindTrEvents();
+        },
+        error: function (error) {
+            alert(error.responseJSON.message);
         }
     });
 }
