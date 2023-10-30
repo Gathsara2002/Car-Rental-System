34dcc6dab3bfb9ca6623fc90cde9a63f5a8e7b4c
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-30 21:16:56 +0530

    Content updated

--- Front_End/controllers/validation/customerValidation.js
+++ Front_End/controllers/validation/customerValidation.js
@@ -5,3 +5,12 @@ const CUS_TEL_REGEX = /^[0-9]{2,}$/;
 const CUS_EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const CUS_NIC_REGEX = /^([0-9]{12}|[0-9V]{10})$/;
 const CUS_LICENSE_REGEX = /^[A-Z0-9-]+$/;
+
+//add validations and text fields to the array
+let c_vArray = [];
+c_vArray.push({field: $("#name"), regEx: CUS_NAME_REGEX, error: 'Name Pattern Is Wrong : A-z'});
+c_vArray.push({field: $("#address"), regEx: CUS_ADDRESS_REGEX, error: 'Address Pattern Is Wrong. : A-z'});
+c_vArray.push({field: $("#contact_No"), regEx: CUS_TEL_REGEX, error: 'Contact Pattern Is Wrong.'});
+c_vArray.push({field: $("#nic"), regEx: CUS_NIC_REGEX, error: 'NIC Pattern Is Wrong.'});
+c_vArray.push({field: $("#license"), regEx: CUS_LICENSE_REGEX, error: 'License Pattern Is Wrong.'});
+c_vArray.push({field: $("#email"), regEx: CUS_EMAIL_REGEX, error: 'Email Pattern Is Wrong.'});
