79426a6538b9931696ca4918be37d51925a7708f
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 12:20:46 +0530

    Added driver db controller script

---
+++ Front_End/controllers/driverDashboardController.js
@@ -0,0 +1,33 @@
+initUi();
+
+/*sidebar operation*/
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+sidebarBtn.onclick = function () {
+    sidebar.classList.toggle("active");
+    if (sidebar.classList.contains("active")) {
+        sidebarBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+    } else
+        sidebarBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+}
+
+/*startup view*/
+function clearDashboard() {
+    $("#dashboard").css('display', 'none');
+    $("#driverSchedule").css('display', 'none');
+}
+
+function initUi() {
+    clearDashboard();
+    $("#dashboard").css('display', 'block');
+}
+
+/*change ui when button clicked*/
+$("#btn-dashboard").click(function () {
+    initUi();
+});
+
+$("#btn-schedule").click(function () {
+    clearDashboard();
+    $("#driverSchedule").css('display', 'block');
+});
