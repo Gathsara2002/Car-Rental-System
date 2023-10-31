81bc794a95317f254e1a905856cf20353c984d9c
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 16:41:32 +0530

    Added rent entity

---
+++ Back_End/src/main/java/lk/easy_car_rental/entity/Rent.java
@@ -0,0 +1,37 @@
+package lk.easy_car_rental.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/31/2023 -- 4:31 PM
+ **/
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Rent {
+
+    @Id
+    private String rId;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private LocalDate returnDate;
+    private LocalDate requestDate;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "cusId", referencedColumnName = "cusId", nullable = false)
+    private Customer customer;
+
+    @OneToMany(mappedBy = "rent", cascade = CascadeType.ALL)
+    private List<RentDetails> rentDetails;
+}
