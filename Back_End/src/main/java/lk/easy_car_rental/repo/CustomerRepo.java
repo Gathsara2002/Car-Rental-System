eda0d4a830d0e52f054f9592539c6899f7548c57
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 20:06:11 +0530

    CustomerRepo updated

--- Back_End/src/main/java/lk/easy_car_rental/repo/CustomerRepo.java
+++ Back_End/src/main/java/lk/easy_car_rental/repo/CustomerRepo.java
@@ -2,10 +2,13 @@ package lk.easy_car_rental.repo;
 
 import lk.easy_car_rental.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : Gathsara
  * created : 10/28/2023 -- 1:50 PM
  **/
 public interface CustomerRepo extends JpaRepository<Customer, String> {
+    @Query(value = "SELECT cusId FROM Customer ORDER BY cusId DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
 }
