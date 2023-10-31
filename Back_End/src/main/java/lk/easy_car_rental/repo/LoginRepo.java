79ab387427fd173ccfffd0c2a5a0216e3b3a27d2
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 19:31:54 +0530

    Added query to  get last userId

--- Back_End/src/main/java/lk/easy_car_rental/repo/LoginRepo.java
+++ Back_End/src/main/java/lk/easy_car_rental/repo/LoginRepo.java
@@ -2,10 +2,13 @@ package lk.easy_car_rental.repo;
 
 import lk.easy_car_rental.entity.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : Gathsara
  * created : 10/27/2023 -- 7:49 PM
  **/
 public interface LoginRepo extends JpaRepository<Login, String> {
+    @Query(value = "SELECT userId FROM Login ORDER BY userId DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
 }
