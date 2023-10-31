3b43121459ca6aada3b34bf0253391a4e40b67b2
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 17:23:16 +0530

    Added rent service impl

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/impl/RentServiceImpl.java
@@ -0,0 +1,29 @@
+package lk.easy_car_rental.service.impl;
+
+import lk.easy_car_rental.repo.RentDetailsRepo;
+import lk.easy_car_rental.repo.RentRepo;
+import lk.easy_car_rental.service.RentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * @author : Gathsara
+ * created : 10/31/2023 -- 5:21 PM
+ **/
+
+@Transactional
+@Service
+public class RentServiceImpl implements RentService {
+
+    @Autowired
+    RentRepo repo;
+
+    @Autowired
+    RentDetailsRepo detailsRepo;
+
+    @Autowired
+    ModelMapper mapper;
+}
