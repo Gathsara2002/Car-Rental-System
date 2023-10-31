ccaeb1cdc8994cfea1e1135002511d6df6acb08a
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 17:28:09 +0530

    PaymentController updated

--- Back_End/src/main/java/lk/easy_car_rental/controller/PaymentController.java
+++ Back_End/src/main/java/lk/easy_car_rental/controller/PaymentController.java
@@ -1,10 +1,13 @@
 package lk.easy_car_rental.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.easy_car_rental.dto.PaymentDTO;
+import lk.easy_car_rental.service.PaymentService;
+import lk.easy_car_rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * @author : Gathsara
@@ -16,4 +19,37 @@ import javax.transaction.Transactional;
 @CrossOrigin
 @Transactional
 public class PaymentController {
+
+    @Autowired
+    PaymentService service;
+
+    @GetMapping
+    public ResponseUtil getAllPayments() {
+        List<PaymentDTO> allPayments = service.getAllPayments();
+        System.out.println(allPayments);
+        return new ResponseUtil("Ok", "Successfully Loaded", service.getAllPayments());
+    }
+
+    @PostMapping
+    public ResponseUtil addPayment(PaymentDTO dto) {
+        service.addPayment(dto);
+        return new ResponseUtil("Ok", "Successfully Added", dto);
+    }
+
+    @DeleteMapping(params = {"payId"})
+    public ResponseUtil deletePayment(String payId) {
+        service.deletePayment(payId);
+        return new ResponseUtil("Ok", "Successfully Deleted", payId);
+    }
+
+    @PutMapping()
+    public ResponseUtil updatePayment(@RequestBody PaymentDTO dto) {
+        service.updatePayment(dto);
+        return new ResponseUtil("Ok", "Successfully Updated", dto);
+    }
+
+    @GetMapping(params = {"payId"})
+    public ResponseUtil findPayment(String payId) {
+        return new ResponseUtil("Ok", "Successfully Searched", service.findPayment(payId));
+    }
 }
