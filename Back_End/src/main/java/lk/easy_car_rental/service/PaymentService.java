553106d3ef7452743cbba8de8d6fb30b09b6926c
Gathsara2002 <liviru2002mmg@gmail.com>
2023-10-31 17:09:10 +0530

    Added payment service

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/PaymentService.java
@@ -0,0 +1,21 @@
+package lk.easy_car_rental.service;
+
+import lk.easy_car_rental.dto.PaymentDTO;
+
+import java.util.List;
+
+/**
+ * @author : Gathsara
+ * created : 10/31/2023 -- 5:07 PM
+ **/
+public interface PaymentService {
+    void addPayment(PaymentDTO dto);
+
+    void deletePayment(String id);
+
+    List<PaymentDTO> getAllPayments();
+
+    PaymentDTO findPayment(String id);
+
+    void updatePayment(PaymentDTO dto);
+}
