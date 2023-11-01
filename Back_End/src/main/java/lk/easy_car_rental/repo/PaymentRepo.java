9e5c5771ca7cc36e0febed70418a89336f2d871b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-01 22:15:22 +0530

    PaymentRepo updated

--- Back_End/src/main/java/lk/easy_car_rental/repo/PaymentRepo.java
+++ Back_End/src/main/java/lk/easy_car_rental/repo/PaymentRepo.java
@@ -2,10 +2,13 @@ package lk.easy_car_rental.repo;
 
 import lk.easy_car_rental.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : Gathsara
  * created : 10/31/2023 -- 5:06 PM
  **/
 public interface PaymentRepo extends JpaRepository<Payment, String> {
+    @Query(value = "SELECT payId FROM Payment ORDER BY payId DESC LIMIT 1", nativeQuery = true)
+    String getLastIndex();
 }
