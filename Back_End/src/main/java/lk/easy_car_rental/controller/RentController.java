405e7efcded1bd0e2343f9d7f8fdd122eeb32e39
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-01 22:18:18 +0530

    RentController updated

--- Back_End/src/main/java/lk/easy_car_rental/controller/RentController.java
+++ Back_End/src/main/java/lk/easy_car_rental/controller/RentController.java
@@ -1,8 +1,10 @@
 package lk.easy_car_rental.controller;
 
 import lk.easy_car_rental.service.RentService;
+import lk.easy_car_rental.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class RentController {
 
     @Autowired
     RentService service;
+
+    @GetMapping(path = "/newId")
+    public ResponseUtil generateNewCusId() {
+        return new ResponseUtil("Ok", "Successfully Id Generated", service.getLastIndex());
+    }
 
 
 }
