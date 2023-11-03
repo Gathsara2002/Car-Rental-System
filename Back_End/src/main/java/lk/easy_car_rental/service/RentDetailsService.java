d716d60f6080fd80fb3b1ff5009eb225335e5474
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-03 23:00:30 +0530

    RentDetailsServices added

---
+++ Back_End/src/main/java/lk/easy_car_rental/service/RentDetailsService.java
@@ -0,0 +1,13 @@
+package lk.easy_car_rental.service;
+
+
+import lk.easy_car_rental.dto.RentDetailsDTO;
+
+/**
+ * @author : Gathsara
+ * created : 11/3/2023 -- 10:57 PM
+ **/
+public interface RentDetailsService {
+
+    void saveRentDetails(RentDetailsDTO dto);
+}
