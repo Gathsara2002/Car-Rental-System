035648fc79cccde215b7df0cc79dda9cfdcb3f2c
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-03 20:11:56 +0530

    rent id generator added

--- Front_End/controllers/rentController.js
+++ Front_End/controllers/rentController.js
@@ -1,4 +1,5 @@
 setDate();
+generateRentId();
 
 /*set date */
 function setDate() {
@@ -8,4 +9,28 @@ function setDate() {
     let day = currentDateTime.getDate();
 
     $("#clock").text(month + "/" + day + "/" + year);
+}
+
+/*set rent id*/
+function generateRentId() {
+    $.ajax({
+        url: BaseUrl + "/rent/newId",
+        method: "get",
+        contentType: "application/json",
+        dataType: "json",
+        success: function (resp) {
+            let newId = resp.data;
+            console.log(newId);
+            if (newId == null) {
+                $("#rentId").val("RNT-001");
+            } else {
+                let tempId = parseInt(newId.split("-")[1]);
+                tempId = tempId + 1;
+                $("#rentId").val("RNT-00" + tempId);
+            }
+        },
+        error: function (error) {
+            console.log(error.message);
+        }
+    });
 }
