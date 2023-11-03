4f457d9865ff50cc4afcafe1600f5eda5d61e6a9
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-03 20:03:30 +0530

    Added rent controller

---
+++ Front_End/controllers/rentController.js
@@ -0,0 +1,11 @@
+setDate();
+
+/*set date */
+function setDate() {
+    let currentDateTime = new Date();
+    let year = currentDateTime.getFullYear();
+    let month = currentDateTime.getMonth() + 1; // Months are 0-based (0 = January)
+    let day = currentDateTime.getDate();
+
+    $("#clock").text(month + "/" + day + "/" + year);
+}
