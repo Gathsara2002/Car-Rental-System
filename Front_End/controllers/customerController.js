cf8df5e46228c8bd79c49c272fcfea8cfe61fb71
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-13 21:45:20 +0530

    Dynamically car added operation is done

--- Front_End/controllers/customerController.js
+++ Front_End/controllers/customerController.js
@@ -1,6 +1,7 @@
 getAllCustomers();
 bindTrEventsToCustomer();
 newCustomerId();
+loadAllCars();
 
 /*to store values temporary*/
 let cusId;
@@ -240,6 +241,75 @@ function loadAllCars() {
                 let registrationNumber = car.regNo;
                 let color = car.color;
                 let vehicleAvailability = car.availability;
+
+                /*set car detail*/
+                $("#allCars").append(
+                    ` <div class="col position-relative mt-5" id="rentCard1" style="border: 1px solid black;">
+                    <div class="carBrand">${brand}</div>
+                    <div class="carImages d-flex mt-3 mb-3 justify-content-between">
+                        <div class="img1"></div>
+                        <div class="img2"></div>
+                        <div class="img3"></div>
+                        <div class="img4"></div>
+
+                    </div>
+                    <div class="carDetails">
+                        <div class="container text-start mt-3 mb-3">
+                            <div class="row row-cols-3">
+                                <div class="col d-flex  mt-2 mb-2 ">
+                                    <div>Car Id :</div>
+                                    <div class="carId">${id}</div>
+                                </div>
+                                <div class="col mt-2 mb-2 ">Color : ${color}</div>
+                                <div class="col mt-2 mb-2 ">Type : ${type}</div>
+                                <div class="col mt-2 mb-2 ">Fuel Type : ${fuelType}</div>
+                                <div class="col mt-2 mb-2 ">Daily Price : ${dailyRate}</div>
+                                <div class="col mt-2 mb-2 ">Monthly Price : ${monthlyRate}</div>
+                                <div class="col mt-2 mb-2 ">Price per Extra Km : ${priceExtraKM}</div>
+                                <div class="col mt-2 mb-2 ">Free Mileage : ${freeMileage}</div>
+                                <div class="col mt-2 mb-2 ">Passenger Count : ${noOfPassengers}</div>
+                                <div class="col mt-2 mb-2 ">Reg No : ${registrationNumber}</div>
+                                <div class="col mt-2 mb-2 ">Transmission Type : ${transmissionType}</div>
+                                <div class="col mt-2 mb-2 ">Availability : ${vehicleAvailability}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="margin-top: 25px;">
+                        <button class="rentButton"> Rent Car</button>
+                    </div>
+                </div>`
+                );
+
+                /*set car images*/
+
+                $(".img1").css({
+                    'background': `url("../${car.frontView}")`,
+                    'background-size': "contain",
+                    ' background-position': "center",
+                    'background-repeat': " no-repeat "
+                });
+
+                $(".img2").css({
+                    'background': `url("../${car.backView}")`,
+                    'background-size': "contain",
+                    ' background-position': "center",
+                    'background-repeat': " no-repeat "
+                });
+
+                $(".img3").css({
+                    'background': `url("../${car.interior}")`,
+                    'background-size': "contain",
+                    ' background-position': "center",
+                    'background-repeat': " no-repeat "
+                });
+
+                $(".img4").css({
+                    'background': `url("../${car.sideView}")`,
+                    'background-size': "contain",
+                    ' background-position': "center",
+                    'background-repeat': " no-repeat "
+                });
+
             }
         },
         error: function (error) {
