4958700ff79ed309711bb202e7f2b3869409647d
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-14 09:06:35 +0530

    Added query to car repo

--- Back_End/src/main/java/lk/easy_car_rental/repo/CarRepo.java
+++ Back_End/src/main/java/lk/easy_car_rental/repo/CarRepo.java
@@ -4,6 +4,8 @@ import lk.easy_car_rental.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
+
 /**
  * @author : Gathsara
  * created : 10/28/2023 -- 7:47 PM
@@ -11,4 +13,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface CarRepo extends JpaRepository<Car, String> {
     @Query(value = "SELECT carId FROM Car ORDER BY carId DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
+
+    @Query(value = "SELECT * FROM Car WHERE transmissionType =?1 and availability='AVAILABLE'", nativeQuery = true)
+    ArrayList<Car> filterByTransmissionType(String transmissionType);
 }
