980e9af15ad00b68b65e6c7254240a4a8aa50cd6
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-15 20:13:05 +0530

    Added query to driver repo

--- Back_End/src/main/java/lk/easy_car_rental/repo/DriverRepo.java
+++ Back_End/src/main/java/lk/easy_car_rental/repo/DriverRepo.java
@@ -4,6 +4,8 @@ import lk.easy_car_rental.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
+
 /**
  * @author : Gathsara
  * created : 10/28/2023 -- 8:51 PM
@@ -11,4 +13,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface DriverRepo extends JpaRepository<Driver, String> {
     @Query(value = "SELECT dId FROM Driver ORDER BY dId DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
+
+    @Query(value = "SELECT * FROM Driver WHERE availability='AVAILABLE'", nativeQuery = true)
+    ArrayList<Driver> getAllAvailableDrivers();
 }
