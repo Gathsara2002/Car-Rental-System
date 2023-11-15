f91410b2cfd5af04f9ff460155af4ca1ecc0bcfd
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-15 21:05:04 +0530

    Rent repo updated

--- Back_End/src/main/java/lk/easy_car_rental/repo/RentRepo.java
+++ Back_End/src/main/java/lk/easy_car_rental/repo/RentRepo.java
@@ -4,11 +4,16 @@ import lk.easy_car_rental.entity.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.ArrayList;
+
 /**
  * @author : Gathsara
  * created : 10/31/2023 -- 5:15 PM
  **/
-public interface RentRepo extends JpaRepository<Rent,String> {
+public interface RentRepo extends JpaRepository<Rent, String> {
     @Query(value = "SELECT rentID FROM Rent ORDER BY rentID DESC LIMIT 1", nativeQuery = true)
     String getLastIndex();
+
+    @Query(value = "SELECT * FROM Rent WHERE status='Pending'", nativeQuery = true)
+    ArrayList<Rent> getAllPendingRents();
 }
